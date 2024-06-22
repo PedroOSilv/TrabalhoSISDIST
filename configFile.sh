9f533511a87acc0f619434cf9cb9b552070213be
Pedro Silva <pedroapsilva22@gmail.com>
2024-06-22 14:05:14 -0400

    excluindo linha repetida

--- configFile.sh
+++ configFile.sh
@@ -3,6 +3,4 @@ sudo apt install -y build-essential autoconf libtool pkg-config
 
 sudo apt install -y python3 python3-venv python3-pip
 
-sudo apt install -y python3 python3-venv python3-pip
-
 pip install grpcio grpcio-tools
