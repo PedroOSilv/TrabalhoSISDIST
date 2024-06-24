bc163c4e37b2b34528b776b8de40a8239a2380e2
Pedro Silva <pedroapsilva22@gmail.com>
2024-06-24 17:58:32 -0400

    download functionality

---
+++ website/scripts/functions.js
@@ -0,0 +1,53 @@
+function deletarArquivo(nomeArquivo) {
+    document.getElementById(nomeArquivo.getAttribute("value")).remove();
+    sessionStorage.removeItem(nomeArquivo.getAttribute("value"));
+}
+
+function downloadFile(fileName) {
+    fileName = fileName.getAttribute("value");
+
+    console.log("Download do arquivo: " + fileName);
+    
+    //fazer requisição para baixar o arquivo via post
+    fetch('/download', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' // Adicionando cabeçalho Content-Type
+        },
+        body: JSON.stringify({ filename: fileName }),
+    })
+        .then(response => response.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(new Blob([blob]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', fileName);
+            document.body.appendChild(link);
+            link.click();
+            link.parentNode.removeChild(link);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            openModalWithMessage("Erro ao baixar arquivo: "+ error);
+        });
+    
+}
+
+function openModalWithMessage(x) {
+    //seta a mensagem do modal
+    document.getElementById("modalMessage").innerHTML = x;
+    $("#myModal").modal("show");
+}
+
+function closeModalWithMessage() {
+    $("#myModal").modal("hide");
+}
+
+
+function openModal(x) {
+    $("#" + x).modal("show");
+}
+
+function closeModal(x) {
+    $("#" + x).modal("hide");
+}
--- website/scripts/script.js
+++ website/scripts/script.js
@@ -25,12 +25,16 @@ async function listarArquivos() {
             for (const file of files) {
 
               tagFile.innerHTML +=
-                  `<span id="` +
+                `<span id="` +
                   file +
                   `" style="margin: 5px;" class="badge d-flex p-2 align-items-center bg-success rounded-pill">
-                <span class="px-1"> ` +
+                
+                <a style="color: aliceblue;" value="` +
                   file+
-                  ` </span>
+                  `" href="#" onclick="downloadFile(this)">
+                  <span class="` +file +`"class="px-1"> ` +
+                  file+
+                  ` </span> </a>
                 <a style="color: aliceblue;" value="` +
                   file+
                   `" href="#" onclick="deletarArquivo(this)">
@@ -59,30 +63,7 @@ async function handleFile(file) {
       console.log("Arquivo carregado com sucesso");
     };
 
-    reader.onerror = reject;
-
-    reader.readAsArrayBuffer(file);
-
-    var tagFile = document.getElementById("files");
-
-    tagFile.innerHTML +=
-      `<span id="` +
-      file.name +
-      `" style="margin: 5px;" class="badge d-flex p-2 align-items-center bg-success rounded-pill">
-    <span class="px-1"> ` +
-      file.name +
-      ` </span>
-    <a style="color: aliceblue;" value="` +
-      file.name +
-      `" href="#" onclick="deletarArquivo(this)">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-    class="bi bi-x-circle" viewBox="0 0 16 16">
-    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-    <path
-    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-    </svg>
-    </a>
-    </span>`;
+    listarArquivos();
   });
 }
 
